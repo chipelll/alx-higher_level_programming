1c4bcb759c5effb600891eed374e6ad7ac6ab1c7
chipelll <chipel009@yahoo.com>
2022-11-29 14:33:44 -0800

    Nice one

---
+++ 0x00-python-hello_world/python.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+with open("/root/alx-higher_level_programming/0x00-python-hello_world/Zen", "r") as f:
+        lines = f.readlines()
+        for i in lines:
+            print (i)
