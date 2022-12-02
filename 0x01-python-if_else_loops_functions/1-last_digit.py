2cae1126f9d3e34e4a924e8462cad814e37da3cc
chipelll <chipel009@yahoo.com>
2022-12-01 23:22:38 -0800

    Print alphabets using ascii value

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
-if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-elif last_digit < 6 and last_digit > 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+nb = random.randint(-10000, 10000)
+ld = int(str(nb)[-1])
+if nb < 0:
+    ld *= -1
+if ld > 5:
+    print("Last digit of {} is {} and is greater than 5".format(nb, ld))
+elif ld == 0:
+    print("Last digit of {} is {} and is 0".format(nb, ld))
 else:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(nb, ld))
