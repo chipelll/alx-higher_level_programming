e5694e86c52da9ef62b35967035cfd517ae94841
chipelll <chipel009@yahoo.com>
2022-12-18 03:55:52 -0800

    Pycodestyle treated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,15 +4,13 @@ import sys
 
 def main():
     i = 0
-    if len(sys.argv)  == 2:
-                print("1 argument:")
+    if len(sys.argv) == 2:
+        print("1 argument:")
     else:
         if len(sys.argv) == 1:
-                print("0 arguments.")
-        else: 
-                print("{} arguments:".format(((len(sys.argv)) - 1)))
-
-                    
+            print("0 arguments.")
+        else:
+            print("{} arguments:".format(((len(sys.argv)) - 1)))
     for i, v in enumerate(sys.argv):
         if i != 0:
             print("{}: {}".format(i, v))
