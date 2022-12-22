ad7e3ee425c255735e02f42bf31941125c59d8f7
chipelll <chipel009@yahoo.com>
2022-12-22 14:23:11 -0800

    Reverse list and print

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,5 +6,5 @@ def replace_in_list(my_list, idx, element):
         return (my_list)
     for i, v in enumerate(my_list):
         if i == idx:
-            my_list[i] = element
+            my_list[idx] = element
             return (my_list)
